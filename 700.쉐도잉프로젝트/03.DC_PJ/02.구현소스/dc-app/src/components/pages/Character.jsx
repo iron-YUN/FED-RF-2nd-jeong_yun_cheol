e256c1ec0cc05f80a09cfc1f315d23fa94d7cf6d
iron-YUN <159221265+iron-YUN@users.noreply.github.com>
2024-06-04 09:48:52 +0900

    gfgf

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Character.jsx
@@ -0,0 +1,9 @@
+// 캐릭터페이지 컴포넌트 ///
+
+export default function Character() {
+    return (
+      <>
+        <h1>캐릭터페이지</h1>
+      </>
+    );
+  } ////////////// Character /////////////
---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Comics.jsx
@@ -0,0 +1,9 @@
+// 코믹스페이지 컴포넌트 ///
+
+export default function Comics() {
+    return (
+      <>
+        <h1>코믹스페이지</h1>
+      </>
+    );
+  } ////////////// Comics /////////////
