69dc76e41db77cefedfe8e146eeefe7e90988b69
iron-YUN <159221265+iron-YUN@users.noreply.github.com>
2024-06-18 18:20:44 +0900

    꺄올

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/SearchPage.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/SearchPage.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Searching from '../modules/Searching';
 
-function SearchPage(props) {
+// 라우터 전달변수값 받기위해
+import { useLocation } from 'react-router-dom';
+
+function SearchPage() {
+    // 라우터 전달값 받기 객체 생성
+    const loc = useLocation();
+    // 넘어온 키워드 받기 ////
+    let keyword = loc.state.keyword;
+
+    console.log("검색어:",keyword)
+
+    // 코드 리턴구역 //////////
     return (
         <>
-            <h1 className='tit'>
-
-            </h1>
-            <Searching/>
-        </>
+        <h1 className='tit'>
+            Search Result
+        </h1>
+        <Searching kword={keyword} />
+    </>
     );
 }
 
