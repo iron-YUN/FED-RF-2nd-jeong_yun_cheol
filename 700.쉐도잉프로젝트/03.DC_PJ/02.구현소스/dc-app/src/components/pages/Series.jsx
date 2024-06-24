89771ea0ddaee0fb5ee4fc3bafcc18c6c6e03762
iron-YUN <wjddbscjf92@naver.com>
2024-06-24 23:40:17 +0900

    빌드업로드

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Series.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Series.jsx
@@ -1,15 +1,20 @@
 // 시리즈 페이지 컴포넌트 ///
 
 import Banner from "../modules/Banner";
+import VidIntro from "../modules/VidIntro";
+import VidSwipe from "../modules/VidSwipe";
 
-export default function Series(){
-
-    //// 코드 리턴구역 //////////////
-    return(
-        <>
-           {/* 1. 배너 컴포넌트 */}
-           <Banner catName="SERIES" />
-        </>
-    );
-
+export default function Series() {
+  //// 코드 리턴구역 //////////////
+  return (
+    <>
+      {/* 1. 배너 컴포넌트 */}
+      <Banner catName="SERIES" />
+      {/* 2. 비디오소개 컴포넌트 */}
+      <VidIntro catName="MOVIES" clsName="on" />
+      {/* 3. 비디오스와이프 컴포넌트 */}
+      <VidSwipe catName="movies" />
+      
+    </>
+  );
 } /////////// Series /////////////////////
