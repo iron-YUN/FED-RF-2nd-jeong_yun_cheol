dd18ca1f228dba43d7e8ec7a47c5b803a63075ee
iron-YUN <159221265+iron-YUN@users.noreply.github.com>
2024-07-01 14:18:37 +0900

    sid

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -11,11 +11,12 @@ import News from "./components/pages/News";
 import Main from "./components/pages/Main";
 import Comics from "./components/pages/Comics";
 import Board from "./components/pages/Board";
+import CatDetail from "./components/pages/CatDetail";
+import SearchPage from "./components/pages/SearchPage";
+import Member from "./components/pages/Member";
 
 // 전체 공통 CSS 불러오기
 import "../src/css/index.scss";
-import CatDetail from "./components/pages/CatDetail";
-import SearchPage from "./components/pages/SearchPage";
 
 /********************************************* 
     [ 리액트 라우터 ]
@@ -81,6 +82,7 @@ export default function MainComponent() {
           <Route path="board" element={<Board />} />
           <Route path="detail" element={<CatDetail />} />
           <Route path="search" element={<SearchPage />} />
+          <Route path="member" element={<Member />} />
         </Route>
         {/* Layout 루트 Route로 하위 Route를 감싼다! */}
       </Routes>
