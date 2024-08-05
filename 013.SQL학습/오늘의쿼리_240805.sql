f4e33ef474e0a2e38d0b6198cd893f5db8ecae52
iron-YUN <159221265+iron-YUN@users.noreply.github.com>
2024-08-05 14:09:26 +0900

    오늘의쿼리 SQL

---
+++ 013.SQL학습/오늘의쿼리_240805.sql
@@ -0,0 +1,12 @@
+-- 주석
+-- DB는 크게 2가지 DDL / DML 
+-- DMK 은 크루드! CRUD creat(insert) / read(select) / update / delete
+-- [select 선택항목 from 테이블명 where 조건절]
+-- SELECT * FROM Customers;
+-- SELECT city FROM Customers;
+-- SELECT ContactName,city FROM Customers;
+-- SELECT ContactName,city FROM Customers where city="Madrid";
+-- like 검색하기 %있는 부분이 자유로운(아무문자) 부분임!
+-- select * from Customers where city like "a%";
+-- select * from Customers where city like "%co%";
+select * from Customers where city like "%n";
